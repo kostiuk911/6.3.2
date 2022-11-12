f1a2385e2a4606eebae2859876312256278fc416
kostiuk911 <115114316+kostiuk911@users.noreply.github.com>
2022-11-12 15:27:46 +0200

    Add files via upload

--- laba6.3.2/Source1.cpp
+++ laba6.3.2/Source1.cpp
@@ -1,36 +1,42 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int Search(int* arr, int size , int i) {
+void fill(double* pMem, int n);
+int findMinIndex(double* pMem, int n);
 
-
-			cout << "[" << arr[i] << "]";
-		if (i < size - 1)
-			return Search(arr, size, i + 1);
-		else
-			cout << endl;
-	
-}
-int Min(int* arr, int size , int i) {
-	int min = arr[0];
-	if (arr[i] < min)
-		min = arr[i];
-	if (i < size - 1)
-		return min;
-		
-	else
-		return Min(arr, size,  i + 1);
-	
+int main()
+{
+    int n;
+    cout << "Enter array size: "; cin >> n;
+    double* pMem = new double[n];
+    cout << "Please fill array: ";
+    fill(pMem, n);
+    cout << "Minimal index is: ";
+    findMinIndex(pMem, n);
+    delete[n] pMem;
 }
 
+void fill(double* pMem, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pMem[i];
+    }
+}
 
-int main() {
-
-	const int size = 20;
-	int arr[size] = { 1,43,21,4,2,6,23,5,29,58,6,5,89,3,2,66,25,34,45 };
-	Search(arr, size ,0);
-	cout << endl;
-
-			cout << "min = " << Min(arr, size, 0) << endl;
-
+int findMinIndex(double* pMem, int n)
+{
+    double min = pMem[0];
+    int minIndex = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (min > pMem[i])
+        {
+            return findMinIndex(pMem, n);
+            minIndex = i;
+        }
+    }
+    cout << min;
+    return minIndex;
 }
